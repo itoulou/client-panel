74275aa9bc1d09bb08358951886ae11ce4965698
Ivan Toulou <touloumbadjiani@gmail.com>
2019-10-14 16:03:39 +0100

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBx2CKzPraDNHbtdyzmcbj72pUXvrYMyGY",
+    authDomain: "clientpanelprod-91152.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-91152.firebaseio.com",
+    projectId: "clientpanelprod-91152",
+    storageBucket: "clientpanelprod-91152.appspot.com",
+    messagingSenderId: "828746158437",
+    appId: "1:828746158437:web:23f9e26fce6735a6c8518c",
+    measurementId: "G-E04MLBL2MW"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBx2CKzPraDNHbtdyzmcbj72pUXvrYMyGY",
+    authDomain: "clientpanelprod-91152.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-91152.firebaseio.com",
+    projectId: "clientpanelprod-91152",
+    storageBucket: "clientpanelprod-91152.appspot.com",
+    messagingSenderId: "828746158437",
+    appId: "1:828746158437:web:23f9e26fce6735a6c8518c",
+    measurementId: "G-E04MLBL2MW"
+  }
 };
 
 /*
